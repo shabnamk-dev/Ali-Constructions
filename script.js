87d458e6daec6865ffbabf44dd85484cfaa69e33
Shabnam Khan <shabnamk1003@gmail.com>
2026-05-16 16:51:32 +0530

    Replace portfolio filter with image popup
    Replaced portfolio filter functionality with image popup feature.

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
 //Portfolio filter
-const buttons=document.querySelectorAll(".filter-btn");
-const item=document.querySelectorAll(".portfolio-item");
 
-buttoms.forEach(btn=>{
-    btn.addEventListener("click",()=>{
-        const filter=btn.getAttribute("data-filter");
+const popup=document.getElementById("popup");
+const popupImg=document.getElementById("popup-img");
+const closeBtn=document.getElementById("close");
 
-        items.forEach(item=>{
-            if(filter==="all"||item.classList.contains(filter)){
-                item.style.display="block";
-
-            }
-            else{
-                item.style.display="none";
-            }
-        });
+document.querySelectorAll(".grid img").forEach(img=>{
+    img.addEventListener("click",()=>{
+        popup.style.display="flex";
+        popupImg.src=img.src;
     });
 });
 
-//scroll animation
-const sections=document.querySelectorAll("section");
-
-window.addEventListener("scroll",()=>{
-    sections.forEach(sec=>{
-        const top=window.scrollY;
-        const offset=sec.offsetTop-200;
-        const height=sec.offsetHeight;
-
-        if(top>=offset && top<offset+height){
-            sec.classList.add("show");
-        }
-    });
-});
+if(closeBtn){
+    closeBtn.onclick=()=>popup.style.display = "none";
+}
